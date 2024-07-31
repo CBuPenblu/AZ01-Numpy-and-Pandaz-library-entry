877021c1c25f47f129d9e8446b5def4350c1b321
CBuPenblu <ikulagins@outlook.com>
2024-07-31 03:07:38 +0300

    home_task_changes

--- home1.py
+++ home1.py
@@ -2,8 +2,11 @@ import pandas as pd
 
 df = pd.read_csv('dogs_dataset.csv')
 
+print("\nПервые 5 строк данных:")
 print(df.head())
 
+print("\nИнформация о данных:")
 print(df.info())
 
+print("\nСтатистическое описание:")
 print(df.describe())
